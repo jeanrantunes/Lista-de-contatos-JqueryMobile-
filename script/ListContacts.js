49a8e9a2ae756bfeececf25e8c2207f130cc0a57
jeanrantunes <jeanrantunes93@gmail.com>
2016-09-27 23:13:42 -0300

    Lista funcionando

---
+++ script/ListContacts.js
@@ -0,0 +1,26 @@
+var ListContacts = function(srcList) {
+    this.srcList = srcList;
+
+    this.getListContacts = function(){
+        var self = this;
+        var list = [];
+        $.getJSON(self.srcList, function(data){
+            list = data;
+        }).done(function(){
+            self.setAttrsContact(list);
+        }).fail(function(){
+            alert("Not found!");
+        });
+    };
+
+    this.setAttrsContact =function(list_contacts) {
+        var list = [];
+
+        $.each(list_contacts, function(){
+            list.push(new Contact(this.Name, this.LastName, this.Photo, this.Email, this.Phone, this.CellPhone, this.Birthday));
+        });
+
+        var adapter = new AdapterList(list);
+        adapter.renderList();
+    };
+};
--- script/test.js
+++ script/test.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-	var list_contacts = $.parseJSON(list);
+	/*var list_contacts = $.parseJSON(list);
 	$('h4').append(list_contacts[0].Email);
-	$('img').attr('src', '../Contacts/'+list_contacts[0].Photo);
+	$('img').attr('src', '../Contacts/'+list_contacts[0].Photo);*/
+	/*$.getJSON("../Contacts/list.json", function(data){
+            list = data;
+        }).done(function(){
+           $('h4').append(list[0].Name);
+        }).fail(function(){
+            $('h4').append("Not found!");
+        });*/
+    var listContacts = new ListContacts("../Contacts/list.json");
+    listContacts.getListContacts(); 
 });
